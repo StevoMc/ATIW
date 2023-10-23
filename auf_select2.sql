74662458834a58a556623756bb8c5d6797b9c385
StevoMc <stevenmc59@gmail.com>
2023-10-23 11:56:18 +0200

    The following changes were made to the auf_select2.sql file:
    - A new query was added to find the smallest women grouped by year of birth.
    - A new query was added to count the number of distinct occupations in the database.
    - A new query was added to count the number of people in the database.
    - A new query was added to find the total weight of people in the database, separated by gender.
    - A new query was added to find duplicate credit card numbers.
    - A new query was added to find the number of people who have a birthday today.
    - A new query was added to find the age of the oldest and youngest person who has a birthday today.
    - A new query was added to find people with the same first and last name.
    - A new query was added to find the most popular car brand among men who work in an auto company. (generated by useblackbox.ai )

--- auf_select2.sql
+++ auf_select2.sql
@@ -68,17 +68,96 @@ WHERE
 GROUP BY
   EXTRACT(YEAR FROM GEBURTSTAG), GROESSECM;
 
+-- Ich brauche eine Liste von der Groeesse der kleinsten Frauen gruppiert nach Geburtsjahr
+SELECT
+  MIN(GROESSECM)                AS GROESSE
+, EXTRACT(YEAR FROM GEBURTSTAG)
+FROM
+  KUNDEN K
+WHERE
+  GESCHLECHT= 'w'
+GROUP BY
+  EXTRACT(YEAR FROM GEBURTSTAG);
+
 -- Wieviele unterschiedliche Berufe sind in der Datenbank gespeichert?
+SELECT
+  COUNT(DISTINCT BERUF)
+FROM
+  KUNDEN K;
 
+SELECT
+  COUNT(BERUF)
+FROM
+  KUNDEN K;
 
--- Wieviel Gewicht (in t) bringenn die Personen auf die Wage, getrennt nach Frauen und M�nnern?
+-- Wieviel Gewicht (in t) bringenn die Personen auf die Wage, getrennt nach Frauen und M nnern?
+SELECT
+  (COUNT(GEWICHTKG) / 1000) AS GEWICHTT
+, GESCHLECHT
+FROM
+  KUNDEN K
+GROUP BY
+  GESCHLECHT;
 
 -- Taucht eine Kreditkartennummer mehrfach auf?
+SELECT
+  KREDITNR
+, COUNT(*)
+FROM
+  KUNDEN K
+HAVING
+  COUNT(*) > 1
+GROUP BY
+  KREDITNR;
 
 -- Wieviele Leute haben heute Geburtstag?
+SELECT
+  COUNT(GEBURTSTAG) ASANZAHL
+FROM
+  KUNDEN K
+WHERE
+  EXTRACT(DAY FROM GEBURTSTAG) = EXTRACT(DAY FROM CURRENT_DATE)
+  AND EXTRACT(MONTH FROM GEBURTSTAG) = EXTRACT(MONTH FROM CURRENT_DATE);
 
--- Wie alt (in Jahren) ist die �lteste/j�nste Person, die heute Geburtstag hat?
+-- Wie alt (in Jahren) ist die  lteste/j nste Person, die heute Geburtstag hat?
+SELECT
+  COUNT(GEBURTSTAG)                                                      AS ANZAHL
+, (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM MIN(GEBURTSTAG))) AS AELTESTER
+, (EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM MAX(GEBURTSTAG))) AS JUENGSTER
+FROM
+  KUNDEN K
+WHERE
+  EXTRACT(DAY FROM GEBURTSTAG) = EXTRACT(DAY FROM CURRENT_DATE)
+  AND EXTRACT(MONTH FROM GEBURTSTAG) = EXTRACT(MONTH FROM CURRENT_DATE);
 
 -- Gibt es Personen mit gleichen Vor und Nachnamen?
+SELECT
+  NACHNAME
+, VORNAME
+, COUNT(*) AS ANZAHL
+FROM
+  KUNDEN K GROUP BYNACHNAME
+, VORNAME
+HAVING
+  COUNT(*) > 1
+ORDER BY
+  ANZAHL DESC, NACHNAME, VORNAME;
 
 -- 2 Eigene Abfragen
+--Welche Fahrzeugmarke wird am meisten genutzt von Männern, die in einer Auto Firma arbeiten?
+SELECT
+  NACHNAME
+, VORNAME
+, GESCHLECHT
+, FIRMA
+, FAHRZEUG
+, COUNT(FAHRZEUG) AS ANZAHL
+FROM
+  KUNDEN K
+WHERE
+  FIRMA LIKE '%Auto%'
+  AND GESCHLECHT= 'm'
+GROUP BY
+  FAHRZEUG, FIRMA, GESCHLECHT, VORNAME, NACHNAME
+ORDER BY
+  ANZAHL DESC
