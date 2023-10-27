a9248bcdae41460dd3d137d3bdb3efde02f3e63b
StevoMc <stevenmc59@gmail.com>
2023-10-27 11:18:17 +0200

    Add files via upload

---
+++ Generics/Fahrzeuge/Fahrzeuge/Fahrzeuge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeuge
+{
+    internal class Fahrzeuge
+    {
+        private string? besitzer;
+        public string? Besitzer { get => besitzer; set => besitzer = value; }
+    }
+}
---
+++ Generics/Fahrzeuge/Fahrzeuge/Fahrzeugflotte.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeuge
+{
+    internal class Fahrzeugflotte<T>
+    {
+        private List<T>? fahrzeuge;
+        public Fahrzeugflotte() => Fahrzeuge = new();
+
+        public override string ToString()
+        {
+            string fahrzeugListe = "";
+            int index = 1;
+            fahrzeuge?.ForEach(e => fahrzeugListe += $"[{index++}]: {e?.ToString()}\n");
+            return fahrzeugListe;
+        }
+
+        public void Erwerben(T fahrzeug)
+        {
+            Fahrzeuge?.Add(fahrzeug);
+        }
+
+        public List<T>? Fahrzeuge { get => fahrzeuge; set => fahrzeuge = value; }
+    }
+}
